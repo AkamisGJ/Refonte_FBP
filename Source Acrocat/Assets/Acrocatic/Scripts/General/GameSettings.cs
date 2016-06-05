cc22ed3b6b62e33c32fcdb4633be011f18a95bde
AkamisGJ <mrtetex@gmail.com>
2016-06-05 10:54:36 +0200

    Project Unity

---
+++ Source Acrocat/Assets/Acrocatic/Scripts/General/GameSettings.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Acrocatic {
+	// Singleton class for the GameSettings.
+	public class GameSettings : MonoBehaviour {
+		private SpriteRenderer invisible_wall;
+		// Public variables
+		[Tooltip("Enable or disable the sprites for the colliders.")]
+		public bool showColliders = false;
+
+		// Static singleton property
+		public static GameSettings Instance { get; private set; }
+		
+		void Awake() {
+			// Save a reference to the GameSettings component as our singleton instance
+			Instance = this;
+		}
+		void Start() {
+			invisible_wall = GetComponent<SpriteRenderer>();
+			invisible_wall.enabled = false;
+		}
+	}
+}
---
+++ Source Acrocat/Assets/Acrocatic/Scripts/General/Pauser.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Sprites;
+using System.Collections;
+
+public class Pauser : MonoBehaviour {
+	private bool paused = false;
+	public SpriteRenderer pause;
+	public SpriteRenderer fond;
+
+	void Start () {
+		pause = GetComponent<SpriteRenderer> ();
+		fond = fond.GetComponent("Fond pause") as SpriteRenderer;
+		//pause.enabled = false;
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(Input.GetKeyDown(KeyCode.P))
+		{
+			paused = !paused;
+		}
+
+		if (paused) {
+			pause.enabled = true;
+			fond.enabled = true;
+			Time.timeScale = 0;
+		}
+			
+		else
+			pause.enabled = false;
+			fond.enabled = false;
+			Time.timeScale = 1;
+	}
+}
